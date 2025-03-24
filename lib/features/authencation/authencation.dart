5171ef2b0ca4fce78d2f41c56cb5c4c07c5fe679
toila4120 <toila4120@gmail.com>
2025-03-24 13:08:56 +0700

    complete login with firebase by email_password

--- lib/features/authencation/authencation.dart
+++ lib/features/authencation/authencation.dart
@@ -1,10 +1,12 @@
-import 'package:dio/dio.dart';
+// import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:movie_app/config/router/app_router.dart';
 import 'package:movie_app/config/theme/theme.dart';
 import 'package:movie_app/core/constants/app_image.dart';
-import 'package:movie_app/core/utils/app_utils.dart';
+// import 'package:movie_app/core/utils/app_utils.dart';
 import 'package:movie_app/core/utils/disable_glow_behavior.dart';
 import 'package:movie_app/core/widget/widget.dart';
 
--- lib/features/authencation/presentation/screen/login_screen.dart
+++ lib/features/authencation/presentation/screen/login_screen.dart
@@ -10,6 +10,94 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   bool _isRememberMe = false;
   bool _isObscureText = true;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String? _errorMessage;
+
+  Future<void> _loginWithEmail() async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      if (userCredential.user != null) {
+        if (mounted) {
+          context.go(AppRouter.homeTabPath);
+        }
+      } else {
+        setState(() {
+          _errorMessage = "Login failed: No user data returned.";
+        });
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = _getErrorMessage(e.code);
+      });
+    } catch (e, stackTrace) {
+      setState(() {
+        _errorMessage = "Unexpected error: $e";
+      });
+    }
+  }
+
+  Future<void> _loginWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) return;
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      if (userCredential.user != null) {
+        print("User logged in with Google: ${userCredential.user!.uid}");
+        if (mounted) {
+          print("Navigating to ${AppRouter.homeTabPath}");
+          context.go(AppRouter.homeTabPath);
+        }
+      } else {
+        setState(() {
+          _errorMessage = "Google login failed: No user data returned.";
+        });
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = _getErrorMessage(e.code);
+      });
+    } catch (e) {
+      setState(() {
+        _errorMessage = "Unexpected error: $e";
+      });
+      print("Detailed error: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  String _getErrorMessage(String code) {
+    switch (code) {
+      case 'user-not-found':
+        return 'No user found for that email.';
+      case 'wrong-password':
+        return 'Wrong password provided.';
+      case 'invalid-email':
+        return 'The email address is invalid.';
+      default:
+        return 'An error occurred. Please try again.';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return AppContainer(
@@ -29,6 +117,7 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             const SizedBox(height: AppPadding.large),
             AppTextField(
+              controller: _emailController,
               hintText: 'Email',
               prefixIcon: Padding(
                 padding: const EdgeInsets.all(AppPadding.small),
@@ -41,6 +130,7 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             const SizedBox(height: AppPadding.large),
             AppTextField(
+              controller: _passwordController,
               hintText: 'Password',
               prefixIcon: Padding(
                 padding: const EdgeInsets.all(AppPadding.small),
@@ -51,16 +141,17 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
               suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _isObscureText = !_isObscureText;
-                    });
-                  },
-                  icon: Image.asset(
-                    _isObscureText ? AppImage.icEyeOff : AppImage.icEye,
-                    height: 20,
-                    width: 20,
-                  )),
+                onPressed: () {
+                  setState(() {
+                    _isObscureText = !_isObscureText;
+                  });
+                },
+                icon: Image.asset(
+                  _isObscureText ? AppImage.icEyeOff : AppImage.icEye,
+                  height: 20,
+                  width: 20,
+                ),
+              ),
               obscureText: _isObscureText,
             ),
             const SizedBox(height: AppPadding.medium),
@@ -110,11 +201,17 @@ class _LoginScreenState extends State<LoginScreen> {
               ],
             ),
             const SizedBox(height: AppPadding.large),
+            if (_errorMessage != null)
+              Padding(
+                padding: const EdgeInsets.only(bottom: AppPadding.medium),
+                child: Text(
+                  _errorMessage!,
+                  style: const TextStyle(color: Colors.red),
+                ),
+              ),
             CustomAppButton(
               padding: const EdgeInsets.symmetric(vertical: AppPadding.medium),
-              onPressed: () {
-                context.go(AppRouter.homeTabPath);
-              },
+              onPressed: _loginWithEmail,
               radius: AppBorderRadius.r8,
               backgroundColor: AppColor.primary200,
               child: const Text(
@@ -160,7 +257,7 @@ class _LoginScreenState extends State<LoginScreen> {
             CustomAppButton(
               padding: const EdgeInsets.symmetric(vertical: AppPadding.medium),
               splashColor: AppColor.greyScale200,
-              onPressed: () {},
+              onPressed: _loginWithGoogle,
               radius: AppBorderRadius.r8,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(AppBorderRadius.r8),
