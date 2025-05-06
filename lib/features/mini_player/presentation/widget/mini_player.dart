08fd8bb9155d6bc7c8bf5b0e55a1acc8c0f26925
toila4120 <toila4120@gmail.com>
2025-05-06 14:36:36 +0700

    :art: Update shadow color opacity method in MiniPlayer widget for consistency in styling

--- lib/features/mini_player/presentation/widget/mini_player.dart
+++ lib/features/mini_player/presentation/widget/mini_player.dart
@@ -73,7 +73,7 @@ class MiniPlayer extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.3),
+            color: Colors.black.withValues(alpha: 0.3),
             blurRadius: 8,
             offset: const Offset(0, 2),
           ),
